8b4b05479b6283425137b1badc63c37745f67473
jichani <jichan7571@naver.com>
2023-07-17 16:40:49 +0900

    edit

---
+++ src/api.js
@@ -0,0 +1,21 @@
+const BASE_PATH = "http://localhost:8080";
+
+export async function rentalNotices() {
+  return await fetch(`${BASE_PATH}/api/rental/notice`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  }).then((res) => res.json());
+}
+
+export async function rentalNoticeDetail(props) {
+  const id = props.querykey[1];
+  return await fetch(`${BASE_PATH}/api/rental/notice-detail/${id}`, {
+    method: "GET,",
+    headers: {
+      "Content-Type": "application/json",
+    }, credentials: "include",
+  }).then((res) => res.json());
+}
--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import router from './router';
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
